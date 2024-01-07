7967d72568f0482ccc99c2418d82bc9a8dd6654d
Palaash Kolhe <kolhepalaash02@gmail.com>
2024-01-07 03:21:16 -0700

    removed comments

--- chrome-extension/scripts/backgroundScript.js
+++ chrome-extension/scripts/backgroundScript.js
@@ -2,11 +2,5 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     chrome.storage.local.remove("cachedTextElements");
     chrome.storage.local.remove("hiddenTextElements");
     chrome.storage.local.remove("parsedElements");
-	// chrome.tabs.sendMessage(tabId, { message: 'tabUpdated', tab: tab });
 });
 
-
-// update when scrolling
-// chrome.tabs.onCommitted.addListener((activeInfo) => {
-//     chrome.tabs.sendMessage(activeInfo.tabId, { message: 'tabUpdated' });
-// });
