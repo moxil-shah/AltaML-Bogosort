9783af038f9fb2b7b35d23f625085a7ae557497f
Moxil Shah <shahmv200216@gmail.com>
2024-01-06 17:53:15 -0700

    scroll features

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,15 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const sliders = document.querySelectorAll('.form-control-range');
-  
-    sliders.forEach(function(slider) {
-      const labelId = `${slider.id}Label`;
-      console.log(labelId);
-      const label = document.getElementById(labelId);
-  
-      slider.addEventListener('input', function() {
-        const sliderValue = slider.value;
-        label.textContent = `${slider.id} | ${sliderValue}%`;
-      });
+let sliderDic = {};
+
+// Function to debounce the input events
+function debounce(func, delay) {
+  let timeoutId;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      func.apply(context, args);
+    }, delay);
+  };
+}
+
+// Function to save sliderDic to Chrome storage
+function saveToStorage() {
+  return new Promise((resolve) => {
+    chrome.storage.sync.set({ sliderDic: sliderDic }, function () {
+      resolve();
     });
   });
-  
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize sliderDic from Chrome storage on extension startup
+  chrome.storage.sync.get("sliderDic", function (result) {
+    if (result.sliderDic) {
+      sliderDic = result.sliderDic;
+    }
+
+    const sliders = document.querySelectorAll(".form-control-range");
+
+    sliders.forEach(function (slider) {
+      const labelId = `${slider.id}Label`;
+      const label = document.getElementById(labelId);
+
+      // Set initial label text based on stored value
+      label.textContent = `${slider.id} | ${sliderDic[slider.id] || 50}%`;
+
+      // Set initial slider value based on stored value
+      slider.value = sliderDic[slider.id] || 50;
+
+      // Debounce the input event listener
+      const debouncedInput = debounce(function () {
+        const sliderValue = slider.value;
+        label.textContent = `${slider.id} | ${sliderValue}%`;
+        sliderDic[slider.id] = sliderValue;
+
+        // Save sliderDic to Chrome storage
+        saveToStorage();
+      }, 5); // Adjust the delay as needed.
+
+      slider.addEventListener("input", debouncedInput);
+    });
+  });
+});
