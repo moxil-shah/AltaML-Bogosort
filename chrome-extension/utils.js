88334570827df053932a4a67646edfcbdac69a8d
Shaishav Shah <shaishav.shah04@gmail.com>
2024-01-06 18:23:43 -0700

    Batch processor

--- chrome-extension/utils.js
+++ chrome-extension/utils.js
@@ -8,17 +8,11 @@ export async function getActiveTabURL() {
   return tabs[0];
 }
 
-/*
-1. Trim and remove the duplicate elements that we have arleady checked (Cache)
-2. Send to batch processor
-3. BEfore making request, .map each node to trimmed text Content
-4. Use the ordering to get back results and blur shit
-*/
-
-class BatchRequestProcessor {
+class CheckBadEngine {
   constructor(batchSize, asyncFunction) {
     this.batchSize = batchSize;
     this.asyncFunction = asyncFunction;
+    this.userThresholds = [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5]
     this.queue = [];
   }
 
@@ -30,24 +24,34 @@ class BatchRequestProcessor {
     }
   }
 
-  // async processQueue(nodeElements=[]) {
+  async processQueue(nodeElements) {
+    // post request with body
+    const results = await fetch(BACKEND_URL + "/predict", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ content: nodeElements.map(e => e.textContent.trim())})
+    }).then(res => res.json());
 
-  //   // post request with body
-  //   const results = await fetch(BACKEND_URL + "/predict", {
-  //     method: 'POST',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({ content: nodeElements.map(e => e.textContent.trim())})
-  //   }).then(res => res.json());
+    for (let i = 0; i < nodeElements.length; i++) {
+      this.blurElementIfBad(results[i], nodeElements[i]);
+    }
+  }
 
-  //   for (let i = 0; i < nodeElements.length; i++) {
-  //     const { }
-  //   }
+  blurElementIfBad(result, element) {
+    // check if any of the results exceed the user thresholds
+    const { _ : { H, H2, HR, OK, S, S3, SH, V, V2 } } = result;
+    const resultArray = [S, H, V, HR, SH, S3, H2, V2];
 
+    // compare to user thresholds
+    const badResults = resultArray.map((r, i) => r > this.userThresholds[i]);
 
-
-  // }
+    // if any of the results are bad, blur the element
+    if (badResults.includes(true)) {
+      element.style.filter = "blur(5px)";
+    }
+  }
 
 }
